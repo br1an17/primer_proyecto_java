f1957bba83c26d168ac7dcbfdb0bf8a820691a13
br1an17 <brianzairpaez@gmail.com>
2025-11-01 15:17:46 -0300

    cambios de intellij

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,22 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.println("Bienvenido a screen mach");
+        System.out.println("Película: Matrix");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        int fechaDeLanzamiento = 1999;
+     boolean incluidoEnElPlan = true;
+     double notaDeLaPelicula = 8.2;
+
+     double media = (8.2 + 6.8 + 9.0)/3;
+     System.out.println(media);
+
+     String sinopsis = """
+             matrix paradoja de la realidad pero tecnologica
+             Fue lanzada en: 
+             """ + fechaDeLanzamiento;
+     System.out.println(sinopsis);
+
+
     }
 }
